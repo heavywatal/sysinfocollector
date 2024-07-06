a32fb2bdd79cdf276a327b60ab0a81f34a595ace
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-07-06 21:19:09 +0900

    :arrow_up: polars 1.0: replace how="outer_coalesce" with coalesce=True

--- src/sice/main.py
+++ src/sice/main.py
@@ -55,7 +55,7 @@ async def create_view() -> HTMLResponse:
     responses = read_responses()
     if config["list"].exists():
         tbl = pl.read_csv(config["list"], separator="\t")
-        tbl = tbl.join(responses, on="id", how="outer_coalesce")
+        tbl = tbl.join(responses, on="id", how="outer", coalesce=True)
     else:
         tbl = responses
     with resources.files("sice").joinpath("view.html").open() as fin:
